460b463e20876bd6821e2e3cec6c8bae6674bc31
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-02 12:40:01 +0300

    added a job_controller with get,post and delete methods

---
+++ app/controllers/job_controller.rb
@@ -0,0 +1,37 @@
+class JobController < AppController
+
+    post '/jobs/create' do
+        data = JSON.parse(request.body.read)
+        begin
+            job = Job.create(data)
+            json_response(code: 201, data: project)
+        rescue => e
+            {error: e.message}.to_json
+        end
+        # json_response(code: 201, data: project)
+        # rescue => e
+        #     json_response(code: 422, data: { error: e.message })
+    end
+
+    get '/jobs' do
+        jobs = Job.all
+        json_response(data: jobs)
+    end
+
+    delete '/jobs/destroy/:id' do
+        begin
+            job = Job.find(self.job_id)
+            job.destroy
+            json_response(data: { message: "project deleted successfully" })
+        rescue => e
+          json_response(code: 422, data: { error: e.message })
+        end
+    end
+
+    private
+
+    def job_id
+        params['id'].to_i
+    end
+
+end
