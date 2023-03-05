bebddceace94ad14216a2e5144944d669afb6ac5
Malcolm-G <malcolm.githuka@gmail.com>
2023-03-05 20:38:05 +0300

    added belongs_to association

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
     has_many :jobs
     has_many :users, through: :jobs
+    belongs_to :primary_user, class_name: 'User', foreign_key: 'project_owner_id'
 
     enum status:[ :CREATED, :ONGOING, :COMPLETED, :CANCELLED ]
 
